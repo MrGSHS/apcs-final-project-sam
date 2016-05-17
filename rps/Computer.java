0a308102da893736869c1d46356e428c336386f2
Samuel Sadkhin 2016 <ssadkhi6@students.d125.org>
2016-05-17 14:10:35 -0500

    added classes to project

---
+++ rps/Computer.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Computer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Computer
+{
+    public Computer()
+    {
+        
+    }
+    
+    public int choose
+}
---
+++ rps/GameRunner.java
@@ -0,0 +1,14 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Class GameRunner - write a description of the class here
+ * 
+ * @author (your name) 
+ * @version (a version number)
+ */
+public class GameRunner extends JApplet
+{
+    
+}
